1d3fb979230789eefaa713a9accee104bb821de7
Justine Matthews <justinematt14@gmail.com>
2022-02-04 10:01:26 -0500

    first commit

---
+++ .history/src/pages/Home_20220203115359.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import Header from "../partials/Header";
+import HeroHome from "../partials/HeroHome";
+import FeaturesHome from "../partials/FeaturesHome";
+import FeaturesBlocks from "../partials/FeaturesBlocks";
+import FeaturesWorld from "../partials/FeaturesWorld";
+import News from "../partials/News";
+import Cta from "../partials/Cta";
+import Footer from "../partials/Footer";
+
+function Home() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+      {/*  Site header */}
+      <Header />
+
+      {/*  Page content */}
+      <main className="grow">
+        {/*  Page sections */}
+        <HeroHome className="bg-yellow bg-opacity-60" />
+        {/* <FeaturesHome /> */}
+        <FeaturesBlocks className="bg-yellow bg-opacity-20" />
+        <FeaturesWorld className="bg-yellow bg-opacity-20" />
+        {/* <News /> */}
+        <Cta />
+      </main>
+
+      {/*  Site footer */}
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
